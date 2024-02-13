f50583199d2240c497b67a22f2089e1581376c43
vishal <lad.vishal7@gmail.com>
2024-02-13 17:53:07 +0530

    first_commit

---
+++ t1.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return  render_template("index.html")
+@app.route("/about")
+def about():
+    name = 'vishal'
+    return render_template("about.html",name= name )
---
+++ tu1.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return  render_template("index.html")
+@app.route("/about")
+def about():
+    name = 'vishal'
+    return render_template("about.html",name= name )
+
+# app.run(debug=True)
+# app.run()
